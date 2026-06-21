a66b7586047142058a0eb4f936a83ba04b63b6e4
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-06-21 04:05:00 +0000

    Sync LeetCode submission - Maximum Ice Cream Bars - Runtime - 43 ms (14.74%), Memory - 80.3 MB (50.37%)

---
+++ solutions/1833-maximum-ice-cream-bars/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+        int ans =0;
+        sort(costs.begin(), costs.end());
+
+        for(int c : costs){
+            if(coins <c) break;
+            coins -= c;
+            ans++;
+        }
+        return ans;
+    }
+};
