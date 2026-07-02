2b650b949dfc1e72129f32daa37db0a9bdd3177c
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-07-02 10:03:38 +0000

    Sync LeetCode submission - Unique Paths - Runtime - 2 ms (21.02%), Memory - 9.4 MB (54.72%)

---
+++ solutions/0062-unique-paths/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m, vector<int>(n, 0));
+        
+        for(int r=0; r<m; r++) dp[r][0] = 1;
+        for(int c=0; c<n; c++) dp[0][c] = 1;
+
+        for(int i=1; i<m; i++){
+            for(int j=1; j<n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+};
