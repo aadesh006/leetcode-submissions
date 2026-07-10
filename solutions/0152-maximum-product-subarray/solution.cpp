428f509669c0db7c6bf0591e0e0c600165c9e156
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-07-10 07:59:38 +0000

    Sync LeetCode submission - Maximum Product Subarray - Runtime - 0 ms (100.00%), Memory - 17.8 MB (24.03%)

---
+++ solutions/0152-maximum-product-subarray/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        if (nums.empty()) return 0;
+
+        int currentMax = nums[0];
+        int currentMin = nums[0];
+        int globalMax = nums[0];
+
+        for (int i = 1; i < nums.size(); i++) {
+            if(nums[i]<0){
+                swap(currentMax, currentMin);
+            }
+            currentMax = max(nums[i], currentMax * nums[i]);
+            currentMin = min(nums[i], currentMin * nums[i]);
+            globalMax = max(globalMax, currentMax);
+        }
+
+        return globalMax;
+    }
+};
