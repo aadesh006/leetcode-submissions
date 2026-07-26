8b08c2a6c483cbee4947b261a2b73791a0443417
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-07-26 10:47:20 +0000

    Sync LeetCode submission - Even Number of Knight Moves - Runtime - 0 ms (100.00%), Memory - 49.6 MB (93.50%)

---
+++ solutions/3996-even-number-of-knight-moves/solution.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool canReach(vector<int>& start, vector<int>& target) {
+        if((start[0] + start[1] + target[0] + target[1])%2 ==0) return true;
+        return false;
+    }
+};
