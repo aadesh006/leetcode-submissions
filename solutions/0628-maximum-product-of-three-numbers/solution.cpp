1ee583991f61ffc07fc6d2f0c1cdd0203cd7a7dc
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-07-31 17:55:43 +0000

    Sync LeetCode submission - Maximum Product of Three Numbers - Runtime - 11 ms (35.16%), Memory - 31.6 MB (49.24%)

--- solutions/0628-maximum-product-of-three-numbers/solution.cpp
+++ solutions/0628-maximum-product-of-three-numbers/solution.cpp
@@ -3,8 +3,8 @@ public:
     int maximumProduct(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        int maxProduct1 = nums[n-1]*nums[n-2]*nums[n-3];
-        int maxProduct2 = nums[0]*nums[1]*nums[n-1];
-        return max(maxProduct1, maxProduct2);
+        int opt1 = nums[n-1]*nums[n-2]*nums[n-3];
+        int opt2= nums[0]*nums[1]*nums[n-1];
+        return max(opt1, opt2);
     }
 };
