0c2284de899fd15968011f8aa5a38434603d8dc9
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-08-02 05:57:47 +0000

    Sync LeetCode submission - Maximum Number of Balloons - Runtime - 0 ms (100.00%), Memory - 9.1 MB (6.87%)

--- solutions/1189-maximum-number-of-balloons/solution.cpp
+++ solutions/1189-maximum-number-of-balloons/solution.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int maxNumberOfBalloons(string text) {
-        int ans = 0;
-
-        while (true) {
-            string word = "balloon";
-            for (char &c : word) {
-                bool found = false;
-                for (char &ch : text) {
-                    if (ch == c) {
-                        ch = '#';
-                        found = true;
-                        break;
-                    }
-                }
-                if (!found) return ans;
-            }
-            ans++;
+        unordered_map<int,int>hashMap;
+        for (char c : text) {
+            hashMap[c]++;
         }
+        int b = hashMap['b'];
+        int a = hashMap['a'];
+        int l = hashMap['l'] / 2;
+        int o = hashMap['o'] / 2;
+        int n = hashMap['n'];
+        
+        return min({b, a, l, o, n});
     }
 };
