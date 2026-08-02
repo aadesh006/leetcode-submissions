770ae07fa75c78ed451e7aeef6faad6693386fa0
Aadesh Chaudhari <aadeshchaudhari14@gmail.com>
2026-08-02 03:01:40 +0000

    Sync LeetCode submission - Maximize Pair Strength Using GCD - Runtime - 904 ms (30.05%), Memory - 37.1 MB (58.78%)

---
+++ solutions/4010-maximize-pair-strength-using-gcd/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    long long maxPairStrength(vector<int>& nums) {
+        int n=nums.size();
+        long long ans=0;
+        for(int i=0; i<n; i++){
+            for(int j =i+1; j<n; j++){
+                long long g= __gcd(nums[i], nums[j]);
+                long long strength = ((long long)nums[i]* nums[j])/(g*g);
+                ans=max(ans, strength);
+            }
+        }
+        return ans;
+    }
+};
